5b4025b37f2f5d147c367051bd3b85920d15e1f7
Barbara Perlik <barbperlik@gmail.com>
2017-04-11 09:27:24 -0400

    overwrite every fifth post title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
     #declare an instance var @posts & assign it a collection of Post objects
     # using the all method, provided by ActiveRecord
     @posts = Post.all
+    @posts.each do |post|
+      post.update!(title: "SPAM") if (post.id % 5 == 1)
+    end
   end
 
   def show
